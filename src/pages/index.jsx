c4b7b9da3f262e0efe3ccb0d4b3804f131d61578
Patrick Kellar <pjkellar@gmail.com>
2023-10-15 10:42:22 -0400

    update index heading

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -229,10 +229,10 @@ export default function Home({ articles }) {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Remote Fullstack Developer specializing in PHP
+            Senior Software Engineer specializing in PHP & Laravel
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-          Remote Full Stack Developer with a strong foundation in relational databases, system administration, and Dev Ops
+            Remote Senior Software Engineer with a strong foundation in relational databases, system administration, and Dev Ops
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
